69ef7b4f1b7b1b1f19395c52cbe9f216735547cd
marcelaorellana <marcelapaz.1985@gmail.com>
2017-12-04 21:50:52 -0300

    vista de los pokemones

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PokeList from './PokeList';
+import './styles/App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PokeList />
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/PokeCell.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/PokeCell.css';
+import sprites from '../assets/sprites.png';
+
+
+const PokeCell = ({ pokeClass }) => {
+  const { id, backgroundPosition } = pokeClass;
+  const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
+
+  return <button style={style} className="poke-cell"></button>
+};
+
+export default PokeCell;
